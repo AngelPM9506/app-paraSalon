2bf4c76bb25a65d914019700448a75fb94c6cac1
Angel Mondragon <parra_mondragon@live.com>
2022-02-23 18:53:29 -0600

    12.0.3

--- controllers/APIController.php
+++ controllers/APIController.php
@@ -9,7 +9,7 @@ use Model\Servicio;
 class APIController{
     public static function index(){
         $servicios = Servicio::all();
-        debuguear($servicios);
+        debuguear(json_encode($servicios));
         echo json_encode($servicios);
     }
     public static function registrar(){
